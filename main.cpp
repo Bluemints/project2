ec982faf69b0d78f201292c4fe464c610cb1159a
mikelhomeless <mthomer1027@gmail.com>
2018-05-01 17:24:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
-void main()
-{
+#include "morse_code.h"
+#include <iostream>
 
+int main() {
+	morse_code m;
+	std::string test = m.encode("Hello");
+	std::cout << test;
+	std::string test2 = m.decode(test);
+	std::cout << test2;
+	return 0;
 }
